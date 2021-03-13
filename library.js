40a0497bf8c5df7a00c8e84791bcaab4e2be9520
Antosik <grig_anton96@mail.ru>
2021-03-13 12:34:21 +0300

    feat: remove unused staticDir, update codestyle

--- library.js
+++ library.js
@@ -1,52 +1,44 @@
 'use strict';
 
-const controllers = require('./lib/controllers');
-const winston = require.main.require('winston');
+const categories = require.main.require('./src/categories');
+const db = require.main.require('./src/database');
 const meta = require.main.require('./src/meta');
 
-const plugin = {};
-
-plugin.init = function (params, callback) {
-	const router = params.router;
-	const hostMiddleware = params.middleware;
-
-	// We create two routes for every view. One API call, and the actual route itself.
-	// Just add the buildHeader middleware to your route and NodeBB will take care of everything for you.
-	router.get('/admin/plugins/category-queue', hostMiddleware.admin.buildHeader, controllers.renderAdminPage);
-	router.get('/api/admin/plugins/category-queue', controllers.renderAdminPage);
-
-	meta.settings.get('category-queue', function (err, settings) {
-		if (err) {
-			winston.error('[plugin/category-queue] Could not retrieve plugin settings!');
-			plugin.settings = {};
-			return;
-		}
-
-		plugin.settings = settings;
-	});
-
-	callback();
+const CategoryQueuePlugin = {
+	settings: {},
 };
 
-plugin.addAdminNavigation = function (header, callback) {
+CategoryQueuePlugin.init = async function (data) {
+	async function renderAdminPage(req, res) {
+		const cids = await db.getSortedSetRange('categories:cid', 0, -1);
+		const categoriesData = await categories.getCategoriesData(cids);
+		const categoriesTree = categories.getTree(categoriesData);
+
+		res.render('admin/plugins/category-queue', { categories: categoriesTree || [] });
+	}
+
+	data.router.get('/admin/plugins/category-queue', data.middleware.admin.buildHeader, renderAdminPage);
+	data.router.get('/api/admin/plugins/category-queue', renderAdminPage);
+
+	CategoryQueuePlugin.settings = await meta.settings.get('category-queue');
+
+	return Promise.resolve();
+};
+
+CategoryQueuePlugin.addAdminNavigation = async function (header) {
 	header.plugins.push({
 		route: '/plugins/category-queue',
 		icon: 'fa-tint',
 		name: 'Category Queue',
 	});
-
-	callback(null, header);
+	return header;
 };
 
-plugin.postQueue = function (postData, callback) {
-	try {
-		if (Object.values(plugin.settings).includes(String(postData.data.cid))) {
-			postData.shouldQueue = true;
-		}
-		callback(null, postData);
-	} catch (error) {
-		callback(error);
+CategoryQueuePlugin.postQueue = async function (postData) {
+	if (Object.values(CategoryQueuePlugin.settings).includes(String(postData.data.cid))) {
+		postData.shouldQueue = true;
 	}
+	return postData;
 };
 
-module.exports = plugin;
+module.exports = CategoryQueuePlugin;
